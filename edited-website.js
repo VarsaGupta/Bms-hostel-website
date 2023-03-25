77926f2ff5d4e8823415d2e6ac4e200a0416f6f2
aaisha <varsagupta07@gmail.com>
2023-03-25 23:28:49 +0530

    first commit

---
+++ edited-website.js
@@ -0,0 +1,113 @@
+var user;
+
+function openpopup()
+{
+       document.getElementById("loginform").reset();   
+       document.getElementById("SignIn").style.visibility= "visible";
+}
+function closepopup()
+{
+        document.getElementById("SignIn").style.visibility= "hidden";
+}
+    
+function opensignup()
+  {
+    document.getElementById("SignUp").style.visibility= "visible";
+  }
+
+function opensignupfromlogin()
+{
+    document.getElementById("signupform").reset();   
+    document.getElementById("SignIn").style.visibility= "hidden"
+    document.getElementById("SignUp").style.visibility= "visible";
+}
+
+function openpopupfromsignup()
+{
+    document.getElementById("loginform").reset();   
+    document.getElementById("SignUp").style.visibility= "hidden";
+    document.getElementById("SignIn").style.visibility= "visible";   
+}
+
+function closesignup()
+{
+    document.getElementById("SignUp").style.visibility = "hidden";
+}
+let bool=1; 
+function validatelogin()
+{
+    user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    const a = (localStorage.getItem(`${user}`));
+
+    if (a === "") {
+        swal("Wrong Details", "Invalid Username or Password", "error");
+        return false;
+        //yaha pe kuch daal dena agar no input to
+      }
+      else {
+    
+    
+        if (a === pass) {
+          swal(
+            "Log In Succesfull!",
+            "Welcome, to the Bookxchange " + user + "!",
+            "success"
+          );
+          bool=0;
+          closepopup();
+          // document.getElementById("submitonlogin").reset();
+          edit_details();
+          // window.location.href = "index.html";
+        }
+        else {
+          swal("Wrong Details", "Invalid Username or Password", "error");
+        }
+      }
+}
+function check(){
+  console.log(bool)
+  if (bool==1)
+  {
+    openpopup()
+  }
+  else{
+    window.location.href = "Accomodation.html";
+    return false;
+  }
+}
+
+function callsignup()
+{
+    let usernmame = document.getElementById("usernamesignup").value;
+    let password = document.getElementById("passwordsignup").value;
+    localStorage.setItem(usernmame,password);
+}
+
+function edit_details()
+{
+    document.getElementById("nav1").style.visibility = "hidden";
+    document.getElementById("nav2").style.visibility = "visible";
+    document.getElementById("name").innerHTML =`${user}`;
+}
+
+function signout() 
+{
+swal({
+  title: 'Are you sure?',
+  text: "You will be Logged Out !",
+  type: 'warning',
+  showCancelButton: true,
+  confirmButtonColor:'#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then(function() {
+  swal(
+    'Signed Out!',
+    'Youhave been signed out',
+    'success'
+    );
+    location.reload();
+})  
+}
+
